9bc9ab079fb04841f391b7115574761c44d19795
2zcory <khanhtri009@gmail.com>
2026-03-14 11:57:52 +0700

    chore: prepare v1.5.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.example.kanjiwidget"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.5.1"
         manifestPlaceholders["appLabel"] = "Kanji Widget"
     }
 
